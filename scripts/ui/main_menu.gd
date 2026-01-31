2bfcb65aafc74f5a6afe9f448ba4def43f009263
Iván Alexander Aráoz Andrade <iaraoz061102@gmail.com>
2026-01-31 00:04:56 -0500

    Add credits screen and navigation from main menu
    Introduced a new credits scene with scrolling text and a dedicated script to handle its animation and return to the main menu. Updated the main menu to include a Credits button and connected it to the new credits scene.

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -14,6 +14,8 @@ func _process(delta: float) -> void:
 func _on_exit_pressed() -> void:
 	get_tree().quit()
 
-
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/levels/level1.tscn")
+
+func _on_credits_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/ui/credits.tscn")
