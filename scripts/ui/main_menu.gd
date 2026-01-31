cdd05395c8b9816b958ed2dd1516f1fad9de1ff5
Iván Alexander Aráoz Andrade <iaraoz061102@gmail.com>
2026-01-31 18:26:37 -0500

    Add menu music and play on main menu load
    Added 'music_menú.wav' and its import file to assets. Updated main_menu.gd to play the new menu music using MusicManager when the main menu scene is loaded.

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -1,10 +1,10 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	MusicManager.play_music(
+		"menu",
+		preload("res://assets/sounds/music/music_menú.wav")
+	)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
