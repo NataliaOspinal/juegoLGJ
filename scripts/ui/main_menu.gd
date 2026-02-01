d9c5f862e30bf0b2de34a27e1ba90db73a0d7582
Iván Alexander Aráoz Andrade <iaraoz061102@gmail.com>
2026-02-01 04:20:57 -0500

    Add menu button SFX and group handling in main menu
    Added hover and click sound effects for main menu buttons by introducing AudioStreamPlayer nodes and connecting button signals to play the respective sounds. Grouped menu buttons using the 'menu_buttons' group for easier signal management.

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -1,12 +1,27 @@
 extends Control
 
+@onready var sfx_hover: AudioStreamPlayer = $SFX_Hover
+@onready var sfx_click: AudioStreamPlayer = $SFX_Click
+
 func _ready() -> void:
 	MusicManager.play_music(
 		"menu",
 		preload("res://assets/sounds/music/music_menú.wav")
 	)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	for btn in get_tree().get_nodes_in_group("menu_buttons"):
+		if btn is BaseButton:
+			btn.mouse_entered.connect(_on_any_button_hover)
+			btn.focus_entered.connect(_on_any_button_hover)
+			btn.pressed.connect(_on_any_button_pressed)
+
+func _on_any_button_hover() -> void:
+	if not sfx_hover.playing:
+		sfx_hover.play()
+
+func _on_any_button_pressed() -> void:
+	sfx_click.play()
+
 func _process(delta: float) -> void:
 	pass
 
